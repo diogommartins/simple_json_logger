abbacdd04a07a9decc9eb9d7d7dfb93224c2b5c1
diogommartins <magalhaesmartins@icloud.com>
2017-03-31 09:58:57 -0300

    Removendo type anotations e movendo para docstrings

--- simple_json_logger/logger.py
+++ simple_json_logger/logger.py
@@ -11,9 +11,6 @@ class StdoutFilter(logging.Filter):
 
 
 class JsonLogger(logging.Logger):
-    default_stream = sys.stdout
-    error_stream = sys.stderr
-
     def __init__(self, name='json_logger',
                  level=logging.DEBUG,
                  serializer=json.dumps,
@@ -34,16 +31,25 @@ class JsonLogger(logging.Logger):
             self.addHandler(self._stderr_handler)
 
     @property
-    def _stdout_handler(self) -> 'logging.StreamHandler':
+    def _stdout_handler(self):
+        """
+        :rtype: logging.StreamHandler
+        """
         handler = self._make_handler(level=logging.DEBUG, stream=sys.stdout)
         handler.addFilter(StdoutFilter())
         return handler
 
     @property
-    def _stderr_handler(self) -> 'logging.StreamHandler':
+    def _stderr_handler(self):
+        """
+        :rtype: logging.StreamHandler
+        """
         return self._make_handler(level=logging.WARNING, stream=sys.stderr)
 
     def _make_handler(self, level, stream) -> 'logging.StreamHandler':
+        """
+        :rtype: logging.StreamHandler
+        """
         handler = logging.StreamHandler(stream)
         handler.setLevel(level)
         handler.setFormatter(JsonFormatter(self.serializer))
