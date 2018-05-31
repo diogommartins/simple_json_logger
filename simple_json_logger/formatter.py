03bd8678ef572da00ed5e1746fe532ca6c43c35f
diogommartins <magalhaesmartins@icloud.com>
2018-05-31 19:29:34 -0300

    Adding the possibility to exclude fields from the formatter output

--- simple_json_logger/formatter.py
+++ simple_json_logger/formatter.py
@@ -21,10 +21,25 @@ FILE_PATH_FIELDNAME = 'file_path'
 
 class JsonFormatter(logging.Formatter):
     level_to_name_mapping = _levelToName
+    default_fields = frozenset([
+        LOG_LEVEL_FIELDNAME,
+        LOGGED_AT_FIELDNAME,
+        LINE_NUMBER_FIELDNAME,
+        FUNCTION_NAME_FIELDNAME,
+        FILE_PATH_FIELDNAME
+    ])
 
-    def __init__(self, serializer):
+    def __init__(self, serializer, exclude_fields=None):
+        """
+        :type serializer: Callable[[Dict], str]
+        :type exclude_fields: Iterable[str]
+        """
         super(JsonFormatter, self).__init__()
         self.serializer = serializer
+        if exclude_fields is None:
+            self.log_fields = self.default_fields
+        else:
+            self.log_fields = self.default_fields - set(exclude_fields)
 
     @staticmethod
     def _default_json_handler(obj):
@@ -38,15 +53,26 @@ class JsonFormatter(logging.Formatter):
         elif callable(obj):
             return obj()
         return str(obj)
+    
+    def formatter_fields_for_record(self, record):
+        """
+        :type record: LogRecord
+        :rtype: Dict
+        """
+        default_fields = (
+            (LOGGED_AT_FIELDNAME, datetime.now().strftime(DATETIME_FORMAT)),
+            (LINE_NUMBER_FIELDNAME, record.lineno),
+            (FUNCTION_NAME_FIELDNAME, record.funcName),
+            (LOG_LEVEL_FIELDNAME, self.level_to_name_mapping[record.levelno]),
+            (FILE_PATH_FIELDNAME, record.pathname)
+        )
+
+        for field, value in default_fields:
+            if field in self.log_fields:
+                yield field, value
 
     def format(self, record):
-        msg = {
-            LOGGED_AT_FIELDNAME: datetime.now().strftime(DATETIME_FORMAT),
-            LINE_NUMBER_FIELDNAME: record.lineno,
-            FUNCTION_NAME_FIELDNAME: record.funcName,
-            LOG_LEVEL_FIELDNAME: self.level_to_name_mapping[record.levelno],
-            FILE_PATH_FIELDNAME: record.pathname
-        }
+        msg = dict(self.formatter_fields_for_record(record))
         if record.flatten:
             if isinstance(record.msg, dict):
                 msg.update(record.msg)
--- simple_json_logger/logger.py
+++ simple_json_logger/logger.py
@@ -27,7 +27,8 @@ class JsonLogger(logging.Logger):
                  stream=None,
                  flatten=False,
                  serializer_kwargs=None,
-                 extra=None):
+                 extra=None,
+                 exclude_fields=None):
         """
         :type name: str
         :type level: int
@@ -41,7 +42,8 @@ class JsonLogger(logging.Logger):
         super(JsonLogger, self).__init__(name, level)
         self.serializer = serializer
         self.flatten = flatten
-        self.formatter = JsonFormatter(self.serializer)
+
+        self.formatter = JsonFormatter(self.serializer, exclude_fields)
 
         if serializer_kwargs is None:
             serializer_kwargs = {}
